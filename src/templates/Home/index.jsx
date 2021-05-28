ccb8a346136e92c8c401a68c4e2c4119a9d8e997
Robson Adriano Ribeiro <robson.adriano@outlook.com.br>
2021-05-28 17:58:05 -0300

    initial

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,88 @@
+import './style.css';
+import { Component } from 'react';
+import { loadPosts } from '../../utils/load-posts';
+import { Posts } from '../../components/Posts';
+import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
+
+class Home extends Component {
+  state = {
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postPerPage: 10
+  }
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const { page, postPerPage } = this.state;
+
+    const postsAndPhotos = await loadPosts();
+    this.setState({
+      posts: postsAndPhotos.slice(page, postPerPage),
+      allPosts: postsAndPhotos
+    });
+  }
+
+  loadMorePosts = () => {
+    const {
+      page,
+      postPerPage,
+      allPosts,
+      posts,
+      searchValue
+    } = this.state;
+
+    const nextPage = page + postPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage })
+  }
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value });
+  }
+
+  render() {
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue ?
+      allPosts.filter(post => {
+        return post.title.toLowerCase().includes(
+          searchValue.toLowerCase()
+        );
+      })
+      : posts;
+
+    return (
+      <section className="container">
+        <div class="search-container">
+          {!!searchValue && (
+            <h1>Search value: {searchValue}</h1>
+          )}
+
+          <TextInput searchValue={searchValue} handleChange={this.handleChange} />
+        </div>
+
+        {filteredPosts.length > 0 && (
+          <Posts posts={filteredPosts} />
+        )}
+
+        < Posts posts={filteredPosts} />
+        <div class="button-container">
+          <Button
+            text="Load more posts"
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts} />
+        </div>
+      </section >
+    );
+  }
+}
+export default Home;
